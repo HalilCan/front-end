66f37eaddbbd09cd9ab95b49e2626b7e40dc0e49
HalilCan <HalilCan@users.noreply.github.com>
2017-06-19 22:27:03 +0300

    Add files via upload

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -1,38 +1,46 @@
 var scr = document.getElementById("main-screen");
 var curArr = [];
-function btnFun() {
-}
+var openPara = 0;
+
+function btnFun() {}
 function clearCalc() {
   var newArr = [];
   curArr = newArr;
   updateScreen();
 }
 function divide() {
-  if (Number.isInteger(curArr[curArr.length - 1])){
-    curArr.push('/');
+  if (Number.isInteger(curArr[curArr.length - 1]) || curArr[curArr.length - 1] == ")") {
+    curArr.push("/");
   } else {
-    curArr[curArr.length - 1] = '/';
-  }  
-  updateScreen();
-}
-function multiply() {
-  if (Number.isInteger(curArr[curArr.length - 1])){
-    curArr.push('*');
-  } else {
-    curArr[curArr.length - 1] = '*';
+      curArr[curArr.length - 1] = "/";
   }
   updateScreen();
 }
-
+function multiply() {
+  if (Number.isInteger(curArr[curArr.length - 1]) || curArr[curArr.length - 1] == ")") {
+    curArr.push("*");
+  } else {
+      curArr[curArr.length - 1] = "*";
+  }
+  updateScreen();
+}
 function delt() {
   var last = curArr[curArr.length - 1];
-  if (Number.isInteger(last)){
-    last = ('' + last).split('');
-    last[last.length - 1] = '';
-    last = parseInt(last.join(''));
-    curArr[curArr.length - 1] = last;
+  if (Number.isInteger(last)) {
+    if (curArr.length == 1 && Math.abs(curArr[0]) - 10 < 0) {
+      curArr = [];
+    } else {
+      last = ("" + last).split("");
+      last[last.length - 1] = "";
+      last = parseInt(last.join(""));
+      curArr[curArr.length - 1] = last;
+    }
   } else {
-    curArr.splice(curArr.length - 1, 1);
+    if (curArr.length == 1) {
+      curArr = [];
+    } else {
+      curArr.splice(curArr.length - 1, 1);
+    }
   }
   updateScreen();
 }
@@ -46,16 +54,22 @@ function eight() {
 }
 
 function addNum(num) {
-  numStr = '' + num;
-  if (!Number.isInteger(curArr[curArr.length - 1]) && curArr[curArr.length - 1] !== '.'){
+  numStr = "" + num;
+  if (
+    !Number.isInteger(curArr[curArr.length - 1]) &&
+    curArr[curArr.length - 1] !== "." &&
+    curArr[curArr.length - 1] !== ")"
+  ) {
     curArr.push(num);
   } else {
-    if (curArr[curArr.length - 1] == '.') {
+    if (curArr[curArr.length - 1] == ".") {
       var last = curArr.length - 1;
-      curArr[last - 1] = curArr[last - 1] + num/10;
+      curArr[last - 1] = curArr[last - 1] + num / 10;
       curArr.splice(last, 1);
     } else {
-      curArr[curArr.length - 1] = parseInt(''+ curArr[curArr.length - 1] + numStr);      
+      curArr[curArr.length - 1] = parseInt(
+        "" + curArr[curArr.length - 1] + numStr
+      );
     }
   }
 }
@@ -64,11 +78,11 @@ function nine() {
   updateScreen();
 }
 function subtract() {
-  if (Number.isInteger(curArr[curArr.length - 1])){
-    curArr.push('-');
+  if (Number.isInteger(curArr[curArr.length - 1]) || curArr[curArr.length - 1] == ")") {
+    curArr.push("-");
   } else {
-    curArr[curArr.length - 1] = '-';
-  }  
+      curArr[curArr.length - 1] = "-";
+  }
   updateScreen();
 }
 function four() {
@@ -84,11 +98,11 @@ function six() {
   updateScreen();
 }
 function add() {
-  if (Number.isInteger(curArr[curArr.length - 1])){
-    curArr.push('+');
+  if (Number.isInteger(curArr[curArr.length - 1]) || curArr[curArr.length - 1] == ")") {
+    curArr.push("+");
   } else {
-    curArr[curArr.length - 1] = '+';
-  }  
+      curArr[curArr.length - 1] = "+";
+  }
   updateScreen();
 }
 function one() {
@@ -104,58 +118,139 @@ function three() {
   updateScreen();
 }
 function paranthesis() {
+  if (
+    (!Number.isInteger(curArr[curArr.length - 1]) ||
+    curArr[curArr.length - 1] == "(") && curArr[curArr.length - 1] != (')')
+  ) {
+    curArr.push("(");
+    openPara += 1;
+  } else {
+    if (openPara > 0) {
+      curArr.push(")");
+      openPara -= 1;
+    }
+  }
+  console.log(curArr);
+  updateScreen();
 }
 function zero() {
   addNum(0);
   updateScreen();
 }
 function point() {
-  curArr.push('.');
+  if (curArr.length == 0 || curArr[curArr.length - 1] != ")") {
+    curArr.push(".");
+  }
   updateScreen();
 }
 function signChange() {
-  if (Number.isInteger(curArr[curArr.length - 1])){
+  if (Number.isInteger(curArr[curArr.length - 1])) {
     curArr[curArr.length - 1] *= -1;
     updateScreen();
   }
 }
 function eql() {
-  console.log(curArr);
+  //console.log(curArr);
   resolveArray();
-  console.log(curArr);
+  //console.log(curArr);
   updateScreen();
 }
+
 function updateScreen() {
   scr.value = curArr.join("");
 }
 
+function resolveParantheses(arr2) {
+  var arr = arr2;
+  
+  if (arr === undefined) {
+    return undefined;
+  }
+  //2. Sweep inner parantheses
+  for (var pi = 0; pi < arr.length; pi++) {
+    if (arr[pi] == "(") {
+      var begindex = pi;
+      console.log('pindex');
+      for (var pend = arr.length - 1; pend > pi; pend--) {
+        if (arr[pi] == ")") {
+          console.log('end');
+          var endex = pend;
+          //Work out this par, read methods more carefully         
+          console.log(pi + ', ' + pend);
+          console.log(arr);
+          arr.splice(begindex, endex - begindex, resolveParantheses(arr));
+        }
+      }
+    }
+  }
+  //3.Now that that's done, sweep the rest as normal.
+  //3a. Sweep multiples
+  for (var i = 0; i < arr.length; i++) {
+    if (!Number.isInteger(arr[i])) {
+      if (arr[i] == "*") {
+        arr[i - 1] = arr[i - 1] * arr[i + 1];
+        arr.splice(i, 2);
+      }
+      if (arr[i] == "/") {
+        arr[i - 1] = arr[i - 1] / arr[i + 1];
+        arr.splice(i, 2);
+      }
+    }
+  }
+  //3b. Sweep additions
+  for (var i = 0; i < arr.length; i++) {
+    if (!Number.isInteger(arr[i])) {
+      if (arr[i] == "+") {
+        arr[i - 1] = arr[i - 1] + arr[i + 1];
+        arr.splice(i, 2);
+      }
+      if (arr[i] == "-") {
+        arr[i - 1] = arr[i - 1] - arr[i + 1];
+        arr.splice(i, 2);
+      }
+    }
+  }
+  //4. Clean head and tail
+  if (arr[arr.length - 1] == ")" ) {
+    arr.splice(arr.length - 1, 1);
+  }
+  if (arr[0] == '(') {
+    arr.splice(0, 1);    
+  }
+  return arr;
+}
+
 function resolveArray() {
+  console.log(resolveParantheses(curArr));
   //1. Clean tail
-  if (!Number.isInteger(curArr[curArr.length - 1])) {
+  if (
+    !Number.isInteger(curArr[curArr.length - 1]) &&
+    curArr[curArr.length !== ")"]
+  ) {
     curArr.splice(curArr.length - 1, 1);
   }
   //2. Sweep multiples
-  for (var i = 0; i < curArr.length; i ++) {
+  for (var i = 0; i < curArr.length; i++) {
     if (!Number.isInteger(curArr[i])) {
-      if (curArr[i] == '*') {
-        curArr[i-1] = curArr[i-1] * curArr[i+1];
+      if (curArr[i] == "*") {
+        curArr[i - 1] = curArr[i - 1] * curArr[i + 1];
         curArr.splice(i, 2);
       }
-      if (curArr[i] == '/') {
-        curArr[i-1] = curArr[i-1] / curArr[i+1];
+      if (curArr[i] == "/") {
+        curArr[i - 1] = curArr[i - 1] / curArr[i + 1];
         curArr.splice(i, 2);
       }
     }
   }
   //3. Sweep additions
-  for (var i = 0; i < curArr.length; i ++) {
+  for (var i = 0; i < curArr.length; i++) {
     if (!Number.isInteger(curArr[i])) {
-      if (curArr[i] == '+') {
-        curArr[i-1] = curArr[i-1] + curArr[i+1];
+      if (curArr[i] == "+") {
+        curArr[i - 1] = curArr[i - 1] + curArr[i + 1];
         curArr.splice(i, 2);
       }
-      if (curArr[i] == '-') {
-        curArr[i-1] = curArr[i-1] - curArr[i+1];
+      if (curArr[i] == "-") {
+        curArr[i - 1] = curArr[i - 1] - curArr[i + 1];
         curArr.splice(i, 2);
       }
     }
---
+++ pomodoroclock/js/index.js
@@ -0,0 +1,133 @@
+var scr = document.getElementById("main-screen");
+var minBox = document.getElementById("minBox");
+var secBox = document.getElementById("secBox");
+var statusBox = document.getElementById('span-status');
+var boxList = document.getElementById('box-list');
+var toggleButton = document.getElementById('toggleButton');
+
+var mins = 0;
+var secs = 0;
+var label = '';
+var paused = true;
+
+var boxes = [];
+
+function parseSecs() {
+  var mins_new = 0;
+  if (secs >= 60) {
+    mins_new = Math.floor(secs / 60);
+    secs = secs % 60;
+  }
+  return [mins_new, secs];
+}
+
+function toggleState() {
+  paused = !paused;
+  mins = minBox.value;
+  secs = secBox.value;
+  
+  if (mins === undefined) {
+    mins = 0;
+  }
+  if (secs === undefined) {
+    secs = 0;
+  }
+  
+  if (paused) {
+    statusBox.innerHTML = ('Timer is paused');
+    toggleButton.value = ('Play');
+  } else {
+    statusBox.innerHTML = ('Time is ticking');
+    toggleButton.value = ('Pause');
+  }
+}
+
+window.setInterval(countDown, 1000);
+
+function countDown() {
+  if (!paused) {
+    if (mins == 0 && secs == 0) {
+      timeOut();
+    } else {
+      if (secs == 0) {
+        mins -= 1;
+        secs = 59;
+      } else {
+        secs -= 1;
+      }
+    }
+    updateDisp();
+  }
+}
+
+function updateDisp() {
+  minBox.value = mins;
+  secBox.value = secs;
+}
+
+function refreshBox() {
+  box = boxes[0];
+  mins = box.minutes;
+  label = box.label;
+  seconds = box.seconds;
+}
+
+function timeOut() {
+  toggleState();
+  alert("Time Out!");
+  if (boxes.length > 1) {
+    boxes.splice(0,1);
+    clearBoxesOnScreen();
+    renderBoxes();
+    refreshBox();
+    updateDisp();
+  }
+}
+
+function genTimeBox (m, s, label) {
+  return {
+    minutes: m, seconds: s, label: label
+  };
+}
+
+function addBox() {
+  var minBoxNew = document.getElementById('minBoxNew');
+  var secBoxNew = document.getElementById('secBoxNew');
+  var labelBoxNew = document.getElementById('labelBoxNew');
+  
+  var minBoxNewR = document.getElementById('minBoxNewR');
+  var secBoxNewR = document.getElementById('secBoxNewR');
+  var labelBoxNewR = document.getElementById('labelBoxNewR');
+  
+  var newBox = genTimeBox(minBoxNew.value, secBoxNew.value, labelBoxNew.value); 
+  var newBoxR = genTimeBox(minBoxNewR.value, secBoxNewR.value, labelBoxNewR.value); 
+  boxes.push(newBox);
+  boxes.push(newBoxR);
+  clearBoxesOnScreen();
+  renderBoxes();
+}
+
+function clearBoxesOnScreen() {
+  boxList.innerHTML = ("");
+}
+
+function deleteBox(index) {
+  boxes.splice(index,1);
+  clearBoxesOnScreen();
+  renderBoxes();
+}
+
+function renderBoxes() {
+  var len = boxes.length;
+  for (var i = 0; i < len; i ++) {
+    var box = boxes[i];
+    var boxLi = document.createElement("LI");
+    var boxMinutes = parseInt(box.minutes, 10) + Math.floor(box.seconds / 60);
+    
+    boxLi.innerHTML = ("<div id = 'timeBlock-" + i + "' class = 'time-block'> <form> <input id = 'minBox-" + i + "' class = 'min-box' type='text' name='mins' value="+ boxMinutes +">:<input id = 'secBox-"+ i +"' class = 'sec-box' type='text' name='secs' value="+ box.seconds%60 +"><input id = 'labelBox-"+ i +"' class = 'label-box' type='text' name='label' value="+ box.label +"> <input id = 'delete-"+ i +"' class = 'delete-btn' type='button' value='X' onclick = 'deleteBox("+i+");'> </form> </div>");
+    
+      
+    boxList.appendChild(boxLi);
+    
+  }
+}
